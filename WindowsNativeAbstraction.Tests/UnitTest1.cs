4870a03f870a2b1a9e601b32dca573e12eb5957f
Alan Zatarain <ingsintitulo@icloud.com>
2023-06-13 21:59:55 -0600

    Added doc for tests

--- WindowsNativeAbstraction.Tests/UnitTest1.cs
+++ WindowsNativeAbstraction.Tests/UnitTest1.cs
@@ -1,7 +1,13 @@
 namespace WindowsNativeAbstraction.Tests;
 
+/// <summary>
+///  This class performs an important function.
+/// </summary>
 public class UnitTest1
 {
+    /// <summary>
+    ///  This method performs an important function.
+    /// </summary>
     [Fact]
     public void Test1()
     {
